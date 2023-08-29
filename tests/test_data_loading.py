cc6f9363d292152c38bb10c9a525cd9c29009d65
Diego Quintana Esteve <diego.quintana.esteve@echobox.com>
2023-08-29 23:18:23 +0100

    added unit test for data loading

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -6,7 +6,7 @@ from src.data_modeling.data_loading import load_mysql_house_details
 from src.utils.exceptions import NotAllInputsAvailableError
 
 
-def test_not_all_columns_available(mocker):
+def test_not_all_columns_available(mocker, monkeypatch):
     """
     test that we raise a NotAllInputsAvailableError when not all the required
     inputs are available
@@ -17,6 +17,8 @@ def test_not_all_columns_available(mocker):
         "pandas.read_sql", return_value=pd.DataFrame(columns=wrong_input_features)
     )
     mocker.patch("sqlalchemy.create_engine", return_value="")
+    monkeypatch.setenv("AWS_RDS_USER", "mock_user")
+    monkeypatch.setenv("AWS_RDS_PASS", "mock_pass")
 
     with pytest.raises(NotAllInputsAvailableError):
         load_mysql_house_details(input_variables=input_features)

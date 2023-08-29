e3a64de9d309bac3a4a0e6f398f8a3756966ac8c
Diego Quintana Esteve <diego.quintana.esteve@echobox.com>
2023-08-29 23:01:42 +0100

    added unit test for data loading

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -1,17 +1,21 @@
 """ Test for the data loading methods"""
+import pandas as pd
 import pytest
 
 from src.data_modeling.data_loading import load_mysql_house_details
 from src.utils.exceptions import NotAllInputsAvailableError
 
 
-def test_not_all_columns_available():
+def test_not_all_columns_available(mocker):
     """
     test that we raise a NotAllInputsAvailableError when not all the required
     inputs are available
     """
-
     input_features = ["listing_id", "floor_m_sqrt"]
+    wrong_input_features = ["listing_id"]
+    mocker.patch(
+        "pandas.read_sql", return_value=pd.DataFrame(columns=wrong_input_features)
+    )
 
     with pytest.raises(NotAllInputsAvailableError):
         load_mysql_house_details(input_variables=input_features)

be80252cb5c5fbdeea16654e782ade55dcfbd19e
Diego Quintana Esteve <diego.quintana.esteve@echobox.com>
2023-11-05 13:14:37 +0000

    Removed unnecessary test

--- tests/utils/test_train_utils.py
+++ tests/utils/test_train_utils.py
@@ -1,21 +1,8 @@
 """ Tests for the train utils methods """
-import pytest
 from mlflow.models import ModelSignature
 from mlflow.types import ColSpec, Schema
 
-from src.utils.exceptions import EmptyModelSignatureError
 from src.utils.train_utils import get_inputs_from_signature
-
-
-def test_load_inputs_signature_empty():
-    """
-    Test that the method raises a exception when the signature is empty
-    """
-    input_schema = Schema(inputs=[])
-    signature = ModelSignature(inputs=input_schema)
-
-    with pytest.raises(EmptyModelSignatureError):
-        get_inputs_from_signature(signature=signature)
 
 
 def test_load_right_inputs_schema():
